3e01992ebe985ec756bc075c9af9ee8ded764d12
GPad <gpadovani@gmail.com>
2017-11-10 14:57:45 +0100

    Downgrade elixir requirements

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pomodoro.Mixfile do
   def project do
     [app: :pomodoro,
      version: "0.1.0",
-     elixir: "~> 1.5",
+     elixir: "~> 1.4",
      escript: escript(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
